e3f33e788082fc79a8c0a0fa7e177b89fdfe5be2
Tim :) <51849321+Tim3399@users.noreply.github.com>
2026-09-09 18:38:21 +0200

    security: redact credentials from debug HTTP logs

--- internal/update/logclient.go
+++ internal/update/logclient.go
@@ -1,12 +1,10 @@
 package update
 
 import (
-	"bytes"
-	"io"
 	"net/http"
+	"net/url"
+	"sort"
 	"strings"
-
-	"github.com/qdm12/ddns-updater/internal/provider/utils"
 )
 
 //go:generate mockgen -destination=mock_$GOPACKAGE/$GOFILE . DebugLogger
@@ -34,6 +32,8 @@ type loggingRoundTripper struct {
 	logger  DebugLogger
 }
 
+const redactedValue = "[REDACTED]"
+
 func (lrt *loggingRoundTripper) RoundTrip(request *http.Request) (
 	response *http.Response, err error,
 ) {
@@ -50,16 +50,16 @@ func (lrt *loggingRoundTripper) RoundTrip(request *http.Request) (
 }
 
 func requestToString(request *http.Request) (s string) {
-	s = request.Method + " " + request.URL.String()
+	s = request.Method + " " + redactURL(request.URL)
 
 	if request.Header != nil {
 		s += " | headers: " + headerToString(request.Header)
 	}
 
-	if request.Body != nil {
-		newBody, bodyString := readAndResetBody(request.Body)
-		request.Body = newBody
-		s += " | body: " + bodyString
+	// Request bodies can contain provider credentials in arbitrary formats.
+	// Do not serialize them into logs.
+	if request.Body != nil && request.Body != http.NoBody {
+		s += " | body: " + redactedValue
 	}
 
 	return s
@@ -72,34 +72,77 @@ func responseToString(response *http.Response) (s string) {
 		s += " | headers: " + headerToString(response.Header)
 	}
 
-	if response.Body != nil {
-		newBody, bodyString := readAndResetBody(response.Body)
-		response.Body = newBody
-		s += " | body: " + bodyString
+	// Response bodies may echo request credentials or session material.
+	if response.Body != nil && response.Body != http.NoBody {
+		s += " | body: " + redactedValue
 	}
 
 	return s
 }
 
 func headerToString(header http.Header) (s string) {
-	headers := make([]string, 0, len(header))
-	for key, values := range header {
+	keys := make([]string, 0, len(header))
+	for key := range header {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+
+	headers := make([]string, 0, len(keys))
+	for _, key := range keys {
+		values := header.Values(key)
+		if isSensitiveName(key) {
+			values = []string{redactedValue}
+		}
 		headerString := key + ": " + strings.Join(values, ",")
 		headers = append(headers, headerString)
 	}
 	return strings.Join(headers, "; ")
 }
 
-func readAndResetBody(body io.ReadCloser) (
-	newBody io.ReadCloser, bodyString string,
-) {
-	b, err := io.ReadAll(body)
-	if err != nil {
-		bodyString = "error reading body: " + err.Error()
-	} else {
-		bodyString = utils.ToSingleLine(string(b))
-		_ = body.Close()
-		newBody = io.NopCloser(bytes.NewBuffer(b))
+func redactURL(input *url.URL) string {
+	if input == nil {
+		return ""
 	}
-	return newBody, bodyString
+
+	redacted := *input
+	if redacted.User != nil {
+		if _, hasPassword := redacted.User.Password(); hasPassword {
+			redacted.User = url.UserPassword(redactedValue, redactedValue)
+		} else {
+			redacted.User = url.User(redactedValue)
+		}
+	}
+
+	query := redacted.Query()
+	for key := range query {
+		if isSensitiveName(key) {
+			query.Set(key, redactedValue)
+		}
+	}
+	redacted.RawQuery = query.Encode()
+
+	return redacted.String()
+}
+
+func isSensitiveName(name string) bool {
+	normalized := strings.ToLower(name)
+	replacer := strings.NewReplacer("-", "", "_", "", ".", "")
+	normalized = replacer.Replace(normalized)
+
+	switch normalized {
+	case "authorization", "proxyauthorization",
+		"cookie", "setcookie",
+		"token", "accesstoken", "refreshtoken", "idtoken",
+		"password", "passwd",
+		"secret", "clientsecret", "consumersecret",
+		"apikey", "key", "consumerkey",
+		"appsecret", "appkey",
+		"xapikey", "xauthtoken",
+		"auth", "credential", "credentials":
+		return true
+	default:
+		return strings.HasSuffix(normalized, "token") ||
+			strings.HasSuffix(normalized, "secret") ||
+			strings.HasSuffix(normalized, "password")
+	}
 }
